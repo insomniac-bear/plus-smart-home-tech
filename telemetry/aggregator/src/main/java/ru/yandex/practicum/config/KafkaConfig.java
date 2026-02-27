81f3053f20e63c076be971d39a8d43ea99153c1d
Kir <tcherepanov.kir@gmail.com>
2026-02-27 20:49:47 +0300

    [sprint-20] добавить aggregator

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -43,7 +44,7 @@ public class KafkaConfig {
         props.put(ConsumerConfig.CLIENT_ID_CONFIG, "aggregator-consumer");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator-group");
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorEventDeserializer.class.getName());
 
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
