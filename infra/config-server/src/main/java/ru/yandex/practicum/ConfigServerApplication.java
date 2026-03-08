bcf1bcad665dcaccde4930b46b057e321f057c82
Kir <tcherepanov.kir@gmail.com>
2026-03-08 15:14:47 +0300

    [sprint-21-fix] удалить явное исключение r2dbc из application

--- infra/config-server/src/main/java/ru/yandex/practicum/ConfigServerApplication.java
+++ infra/config-server/src/main/java/ru/yandex/practicum/ConfigServerApplication.java
@@ -2,11 +2,10 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication(exclude = R2dbcAutoConfiguration.class)
+@SpringBootApplication
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
