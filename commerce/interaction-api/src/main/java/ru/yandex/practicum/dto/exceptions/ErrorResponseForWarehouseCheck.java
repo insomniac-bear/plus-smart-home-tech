5644dfcc5039bf366c6a4f58f4dd4d145255840b
Kir <tcherepanov.kir@gmail.com>
2026-03-20 23:53:51 +0300

    [sprint-21] fix исправить ошибку наследования класса ошибки

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/exceptions/ErrorResponseForWarehouseCheck.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/exceptions/ErrorResponseForWarehouseCheck.java
@@ -1,15 +1,15 @@
 package ru.yandex.practicum.dto.exceptions;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 public class ErrorResponseForWarehouseCheck extends ErrorResponse {
     private List<UUID> missingProducts;
 }
