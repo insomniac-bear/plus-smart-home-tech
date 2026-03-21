eaaf20e8e0af7d2355e94655dca08b6f0eaf568c
Kir <tcherepanov.kir@gmail.com>
2026-03-21 15:26:02 +0300

    [sprint-21] fix исправить ошибку наследования класса ошибки

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApplication.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/ShoppingStoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableFeignClients(basePackages = "ru.yandex.practicum.client")
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interaction-api.client")
 public class ShoppingStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStoreApplication.class, args);
--- commerce/warehouse/src/main/java/ru/yandex/practicum/WarehouseApplication.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/WarehouseApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableFeignClients(basePackages = "ru.yandex.practicum.client")
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interaction-api.client")
 public class WarehouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
